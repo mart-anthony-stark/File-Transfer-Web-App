58b389e7b167cc40d364fdca53c5213e854dde49
mart-anthony-stark <martsalazar0@gmail.com>
2022-05-31 22:27:23 +0800

    Add: socket

--- server.js
+++ server.js
@@ -6,28 +6,39 @@ const server = require("http").createServer(app);
 
 const io = require("socket.io")(server);
 
-app.use(express.static(path.join(__dirname+"/public")));
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname + "/public")));
 
-io.on("connection", function(socket){
-	socket.on("sender-join",function(data){
-		socket.join(data.uid);
-	});
-	socket.on("receiver-join",function(data){
-		socket.join(data.uid);
-		socket.in(data.sender_uid).emit("init", data.uid);
-	});
-	socket.on("file-meta",function(data){
-		socket.in(data.uid).emit("fs-meta", data.metadata);
-	});
-	socket.on("fs-start",function(data){
-		socket.in(data.uid).emit("fs-share", {});
-	});
-	socket.on("file-raw",function(data){
-		socket.in(data.uid).emit("fs-share", data.buffer);
-	})
+app.get("/", (req, res) => {
+  res.render("start");
+});
+app.get("/send", (req, res) => {
+  res.render("sender");
+});
+app.get("/receive", (req, res) => {
+  res.render("receiver");
 });
 
-const PORT = process.env.PORT || 3000
-server.listen(PORT, ()=>{
-	console.log(`Server running on port ${PORT}`)
+io.on("connection", function (socket) {
+  socket.on("sender-join", function (data) {
+    socket.join(data.uid);
+  });
+  socket.on("receiver-join", function (data) {
+    socket.join(data.uid);
+    socket.in(data.sender_uid).emit("init", data.uid);
+  });
+  socket.on("file-meta", function (data) {
+    socket.in(data.uid).emit("fs-meta", data.metadata);
+  });
+  socket.on("fs-start", function (data) {
+    socket.in(data.uid).emit("fs-share", {});
+  });
+  socket.on("file-raw", function (data) {
+    socket.in(data.uid).emit("fs-share", data.buffer);
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });

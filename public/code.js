63d2c67bd215fe25b6d1c8a44846364a29c780d8
mart-anthony-stark <martsalazar0@gmail.com>
2022-05-31 21:32:46 +0800

    Add: sender script

--- public/code.js
+++ public/code.js
@@ -1,82 +1,73 @@
-(function () {
-  let receiverID;
-  const socket = io();
+(function(){
 
-  function generateID() {
-    return `${Math.trunc(Math.random() * 999)}-${Math.trunc(
-      Math.random() * 999
-    )}-${Math.trunc(Math.random() * 999)}`;
-  }
+	let receiverID;
+	const socket = io();
 
-  document
-    .querySelector("#sender-start-con-btn")
-    .addEventListener("click", () => {
-      let joinID = generateID();
-      document.querySelector("#join-id").innerHTML = `
-        <b>Room ID</b>
-        <span>${joinID}</span>
-      `;
+	function generateID(){
+		return `${Math.trunc(Math.random()*999)}-${Math.trunc(Math.random()*999)}-${Math.trunc(Math.random()*999)}`;
+	}
 
-      socket.emit("sender-join", {
-        uid: joinID,
-      });
-    });
+	document.querySelector("#sender-start-con-btn").addEventListener("click",function(){
+		let joinID = generateID();
+		document.querySelector("#join-id").innerHTML = `
+			<b>Room ID</b>
+			<span>${joinID}</span>
+		`;
+		socket.emit("sender-join", {
+			uid:joinID
+		});
+	});
 
-  socket.on("init", (uid) => {
-    receiverID = uid;
-    document.querySelector(".join-screen").classList.remove("active");
-    document.querySelector(".fs-screen").classList.add("active");
-  });
+	socket.on("init",function(uid){
+		receiverID = uid;
+		document.querySelector(".join-screen").classList.remove("active");
+		document.querySelector(".fs-screen").classList.add("active");
+	});
 
-  document.querySelector("#file-input").addEventListener("change", (e) => {
-    let file = e.target.files[0];
-    if (!file) return;
+	document.querySelector("#file-input").addEventListener("change",function(e){
+		let file = e.target.files[0];
+		if(!file){
+			return;		
+		}
+		let reader = new FileReader();
+		reader.onload = function(e){
+			let buffer = new Uint8Array(reader.result);
 
-    let reader = new FileReader();
-    reader.onload = (ev) => {
-      let buffer = new Uint8Array(reader.result);
-      let el = document.createElement("div");
-      el.classList.add("item");
-      el.innerHTML = `
-        <div class="progress">0%</div>
-        <div class="filename">${file.name}</div>
-      `;
-      document.querySelector(".files-list").appendChild(el);
-      shareFile(
-        {
-          filename: file.name,
-          total_buffer_size: buffer.length,
-          buffer_size: 1024,
-        },
-        buffer,
-        el.querySelector(".progress")
-      );
-    };
+			let el = document.createElement("div");
+			el.classList.add("item");
+			el.innerHTML = `
+					<div class="progress">0%</div>
+					<div class="filename">${file.name}</div>
+			`;
+			document.querySelector(".files-list").appendChild(el);
+			shareFile({
+				filename: file.name,
+				total_buffer_size:buffer.length,
+				buffer_size:1024,
+			}, buffer, el.querySelector(".progress"));
+		}
+		reader.readAsArrayBuffer(file);
+	});
 
-    reader.readAsArrayBuffer(file);
-  });
 
-  function shareFile(metadata, buffer, progress_node) {
-    socket.emit("file-meta", {
-      uid: receiverID,
-      metadata,
-    });
-    socket.on("fs-share", () => {
-      let chunk = buffer.slice(0, metadata.buffer_size);
-      buffer = buffer.slice(metadata.buffer_size, buffer.length);
-      progress_node.innerText =
-        Math.trunc(
-          ((metadata.total_buffer_size - buffer.length) /
-            metadata.total_buffer_size) *
-            100
-        ) + "%";
-
-      if (chunk.length != 0) {
-        socket.emit("file-raw", {
-          uid: receiverID,
-          buffer: chunk,
-        });
-      }
-    });
-  }
+	function shareFile(metadata,buffer,progress_node){
+		socket.emit("file-meta", {
+			uid:receiverID,
+			metadata:metadata
+		});
+		
+		socket.on("fs-share",function(){
+			let chunk = buffer.slice(0,metadata.buffer_size);
+			buffer = buffer.slice(metadata.buffer_size,buffer.length);
+			progress_node.innerText = Math.trunc(((metadata.total_buffer_size - buffer.length) / metadata.total_buffer_size * 100));
+			if(chunk.length != 0){
+				socket.emit("file-raw", {
+					uid:receiverID,
+					buffer:chunk
+				});
+			} else {
+				console.log("Sent file successfully");
+			}
+		});
+	}
 })();

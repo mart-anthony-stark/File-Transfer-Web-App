06e56cd93fad63f26328a61d2dac17bc8e8778e0
mart-anthony-stark <martsalazar0@gmail.com>
2022-05-31 19:22:43 +0800

    Add: socket listeners

--- server.js
+++ server.js
@@ -16,6 +16,15 @@ io.on("connection", (socket) => {
     socket.join(data.uid);
     socket.in(data.sender_uid).emit("init", data.uid);
   });
+  socket.on("fs-meta", (data) => {
+    socket.in(data.uid).emit("fs-meta", data.metadata);
+  });
+  socket.on("fs-start", (data) => {
+    socket.in(data.uid).emit("fs-share", {});
+  });
+  socket.on("file-raw", (data) => {
+    socket.in(data.uid).emit("fs-share", data.buffer);
+  });
 });
 
 server.listen(PORT, () => {

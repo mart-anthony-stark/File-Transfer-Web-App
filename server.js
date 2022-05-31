1690e8f5b3adf3c27fe0bc517daf4dc68d356237
mart-anthony-stark <martsalazar0@gmail.com>
2022-05-31 21:32:38 +0800

    Add: server script

--- server.js
+++ server.js
@@ -1,32 +1,33 @@
 const express = require("express");
 const path = require("path");
+
 const app = express();
 const server = require("http").createServer(app);
 
-const PORT = process.env.PORT || 3000;
 const io = require("socket.io")(server);
 
-app.use(express.static(path.join(__dirname + "/public")));
+app.use(express.static(path.join(__dirname+"/public")));
 
-io.on("connection", (socket) => {
-  socket.on("sender-join", (data) => {
-    socket.join(data.uid);
-  });
-  socket.on("receiver-join", (data) => {
-    socket.join(data.uid);
-    socket.in(data.sender_uid).emit("init", data.uid);
-  });
-  socket.on("fs-meta", (data) => {
-    socket.in(data.uid).emit("fs-meta", data.metadata);
-  });
-  socket.on("fs-start", (data) => {
-    socket.in(data.uid).emit("fs-share", {});
-  });
-  socket.on("file-raw", (data) => {
-    socket.in(data.uid).emit("fs-share", data.buffer);
-  });
+io.on("connection", function(socket){
+	socket.on("sender-join",function(data){
+		socket.join(data.uid);
+	});
+	socket.on("receiver-join",function(data){
+		socket.join(data.uid);
+		socket.in(data.sender_uid).emit("init", data.uid);
+	});
+	socket.on("file-meta",function(data){
+		socket.in(data.uid).emit("fs-meta", data.metadata);
+	});
+	socket.on("fs-start",function(data){
+		socket.in(data.uid).emit("fs-share", {});
+	});
+	socket.on("file-raw",function(data){
+		socket.in(data.uid).emit("fs-share", data.buffer);
+	})
 });
 
-server.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+const PORT = process.env.PORT || 3000
+server.listen(PORT, ()=>{
+	console.log(`Server running on port ${PORT}`)
 });

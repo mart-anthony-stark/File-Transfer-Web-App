5c9890a5cfad265a893fda9cb8da41830218e0df
mart-anthony-stark <martsalazar0@gmail.com>
2022-05-31 19:15:35 +0800

    Add: socket connection

--- server.js
+++ server.js
@@ -4,8 +4,19 @@ const app = express();
 const server = require("http").createServer(app);
 
 const PORT = process.env.PORT || 3000;
+const io = require("socket.io")(server);
 
 app.use(express.static(path.join(__dirname + "/public")));
+
+io.on("connection", (socket) => {
+  socket.on("sender-join", (data) => {
+    socket.join(data.uid);
+  });
+  socket.on("receiver-join", (data) => {
+    socket.join(data.uid);
+    socket.in(data.sender_uid).emit("init", data.uid);
+  });
+});
 
 server.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
